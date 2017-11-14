dd7fbf72b104f7604fa054ea1b658e939ab6f481
isayme <isaymeorg@gmail.com>
2017-11-14 19:48:34 +0800

    use error.status

--- app/middleware/error-handler.js
+++ app/middleware/error-handler.js
@@ -1,5 +1,7 @@
 module.exports = (err, req, res, next) => {
-  res.status(500).json({
+  let status = err.status || 500
+
+  res.status(status).json({
     name: err.name,
     message: err.message,
     stack: err.stack
